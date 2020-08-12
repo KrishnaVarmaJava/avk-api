a56c35f24ee256b78da1a4ec309b76d850ae40ca
Krishna Verma <krishna.verma@yan3k.com>
2020-08-12 16:40:59 +0800

    settings for heroku

--- accessControlApp/settings.py
+++ accessControlApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'g)ehy4gq+s+c($x#v03=wcesijt=+o5!mdcn9unxsxc=0l+6nf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
