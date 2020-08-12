781e31e63353f4041c024507a1faca065002bfe0
Krishna Verma <krishna.verma@yan3k.com>
2020-08-12 16:30:24 +0800

    my first commit

---
+++ agentWebApp/admin.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from models import agentDetails
+
+admin.site.register(agentDetails)
+
+# Register your models here.
