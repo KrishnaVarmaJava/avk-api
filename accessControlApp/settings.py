0c9a1b1cbf33a7a4a65ec53a4021fd178d586b99
Krishna Verma <krishna.verma@yan3k.com>
2020-08-12 16:59:59 +0800

    try to run the app

--- accessControlApp/settings.py
+++ accessControlApp/settings.py
@@ -115,9 +115,15 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Media
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+MEDIA_URL = '/media/'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
